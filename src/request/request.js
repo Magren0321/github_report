25928673b259c895b02d48b390eaca9503699645
Magren <44224328+Magren0321@users.noreply.github.com>
2021-06-08 15:07:22 +0800

    create

---
+++ src/request/request.js
@@ -0,0 +1,42 @@
+import axios from './index';
+
+const githubApi = 'https://api.github.com';
+const myApi = 'http://119.23.216.219:4000';
+
+const request = {
+    /**
+     * 获取github用户的Contributions
+     * @param {string} name 
+     * @returns 
+     */
+    getContributions(name){
+        return axios({
+            url:myApi+'/getgithub?name='+name,
+            method:'get'
+        })
+    },
+    /**
+     * 获取github用户的个人信息
+     * @param {string} name 
+     * @returns 
+     */
+    getInfo(name){
+        return axios({
+            url:githubApi+'/users/'+name,
+            method:'get'
+        })
+    },
+    /**
+     * 获取github用户公开的项目详情
+     * @param {string} name 
+     * @returns 
+     */
+    getRep(name){
+        return axios({
+            url:githubApi+'/users/'+name+'/subscriptions',
+            method:'get'
+        })
+    }
+}
+
+export default request;
